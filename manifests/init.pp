17b3e33f878d74625281d3e61f6db0f7f3e3240c
mh <mh@immerda.ch>
2008-06-13 19:05:33 +0000

    Merge commit 'puzzle/development'

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,18 @@
-#######################################
+#
 # iptables module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-#######################################
-
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "iptables": }
+
 class iptables {
     include iptables::base 
 }
